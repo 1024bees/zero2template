86e7baaf5b98a30ea7f2ee55accfddc8d871124d
James Connolly <jimmy@1024bees.com>
2023-05-28 00:04:57 -0400

    nudge workflow

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use {{ crate_name }}::configuration::get_configuration;
 use {{ crate_name }}::startup::Application;
 use {{ crate_name }}::telemetry::{get_subscriber, init_subscriber};
 
+
 #[tokio::main]
 async fn main() -> Result<(), hyper::Error> {
     let subscriber = get_subscriber("{{ crate_name }}".into(), "info".into(), std::io::stdout);
